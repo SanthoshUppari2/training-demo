c4a3b0b1b8f94bd436b342226848e853b91d6c7c
Santhosh-U <santhosh.u@ddistechnologies.com>
2022-02-28 11:14:59 +0530

    Initial project setup

---
+++ src/main/java/com/usk/uskpay/UskPayApplication.java
@@ -0,0 +1,13 @@
+package com.usk.uskpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UskPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UskPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usk/uskpay/UskPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usk.uskpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UskPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
